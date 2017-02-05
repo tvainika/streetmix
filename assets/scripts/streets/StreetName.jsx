70859af64c4058d1210263885b053d3bc6d8e7bd
Lou Huang <lou@louhuang.com>
2017-02-05 15:22:03 -0500

    Add app settings to Redux store; connect StreetNameCanvas component

--- assets/scripts/streets/StreetName.jsx
+++ assets/scripts/streets/StreetName.jsx
@@ -75,7 +75,7 @@ export default class StreetName extends React.Component {
   }
 
   clickStreetName () {
-    if (!this.props.allowEditing) {
+    if (!this.props.editable) {
       return
     }
 
@@ -106,7 +106,7 @@ export default class StreetName extends React.Component {
 
 StreetName.propTypes = {
   id: React.PropTypes.string,
-  allowEditing: React.PropTypes.bool,
+  editable: React.PropTypes.bool,
   street: React.PropTypes.any,
   handleResize: React.PropTypes.func
 }
